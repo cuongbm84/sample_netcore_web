4b7c5e1ab0fc00d213ee5676dc21051d628e06ed
buiminhcuong <buiminhcuong@hotmail.com>
2019-12-25 13:38:33 +0700

    this test causes build fail

--- Web/Program.cs
+++ Web/Program.cs
@@ -20,7 +20,7 @@ namespace Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
                 });
     }
 }
